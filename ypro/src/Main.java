78d42499fe92240ec736655c37258a833bb39433
Kanat77 <021214550683@enu.kz>
2021-11-09 14:37:09 +0600

    MAin calss plus dannye

--- ypro/src/Main.java
+++ ypro/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("dok");
+        int a=5;
+        double b=8.5;
+        double c=a+b;
+        System.out.println(c);
     }
 }
